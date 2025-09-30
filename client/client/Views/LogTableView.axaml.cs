482a35e21b5a9a0854af251055ec98e9fcf9fe50
Echo_1-1 <fya17102007@gmail.com>
2025-09-30 23:47:20 +0500

    Созданы файлы View и ViewModel для табличного вида логов

---
+++ client/client/Views/LogTableView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace client.Views;
+
+public partial class LogTableView : UserControl
+{
+    public LogTableView()
+    {
+        InitializeComponent();
+    }
+}
