019387a09cb858c8be6f7ec3a3f2ef3e9ab8b61d
Vlad-Vershinin <vladversinin676@gmail.com>
2025-09-30 22:59:56 +0500

    structure created. Closes #9

---
+++ server/Controllers/LogController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("api/[controler]")]
+public class LogController : ControllerBase
+{
+}
---
+++ server/Core/Interfaces/IParserService.cs
@@ -0,0 +1,5 @@
+﻿namespace server.Core.Interfaces;
+
+public interface IParserService
+{
+}
