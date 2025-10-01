768413b8857446a487878a32e357aae5451b1136
Echo_1-1 <fya17102007@gmail.com>
2025-10-01 20:31:18 +0500

    UI changes

--- client/client/Models/ParsedLog.cs
+++ client/client/Models/ParsedLog.cs
@@ -1,5 +1,6 @@
 
 using Newtonsoft.Json.Linq;
+using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -17,10 +18,13 @@ public class ParsedLog
     public string RawJSON { get; }
     [Required]
     public DateTime Timestamp { get; set; }
+    public string TimestampStr { get { return Timestamp.ToLongTimeString(); } }
     [Required]
     public string Level { get; set; } = string.Empty; // ['info', 'debug', 'trace', 'warn', 'error', '@level missed']
     public string Message { get; set; } = string.Empty;
     public JObject OtherKeys { get { return other_keys; } set { this.other_keys = value; } } // это в бд не суём
+
+    
     public bool IsHidden { get; set; } = false;
     public bool IsAnomaly { get; set; } = false;
     public List<ParsedLog> GroupedLogs { get; set; } // логи из этого листа распоковываем как отдельные записи в бд
--- client/client/ViewModels/LogTableViewModel.cs
+++ client/client/ViewModels/LogTableViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Controls.Models.TreeDataGrid;
+using Avalonia.Layout;
+using Avalonia.Media;
+using client.ViewModels.Charts;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using Splat.ModeDetection;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,6 +13,13 @@ using System.Linq;
 using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+
+
+
+
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.VisualElements;
 
 namespace client.ViewModels
 {
@@ -20,6 +31,23 @@ namespace client.ViewModels
         public ReactiveCommand<Unit, Unit> OpenOptionPane { get; set; }
 
 
+
+
+        public ReactiveCommand<Unit, Unit> NextPage { get; set; }
+        public ReactiveCommand<Unit, Unit> PreviousPage { get; set; }
+        public ReactiveCommand<Unit, Unit> FirstPage { get; set; }
+        public ReactiveCommand<Unit, Unit> LastPage { get; set; }
+
+
+        [Reactive]
+        public int CurrentPage { get; set; }
+        public int MaxPage { get; set; } = 100;
+
+
+
+
+
+
         [Reactive]
         public bool IsOptionPaneIsOpen { get; set; } = false;
 
@@ -27,31 +55,86 @@ namespace client.ViewModels
         private ObservableCollection<ParsedLog> _parsedLogs;
         public HierarchicalTreeDataGridSource<ParsedLog> LogsSource { get; }
 
+        public TestChartViewModel TestChart { get; set; } = new TestChartViewModel();
+
 
         public LogTableViewModel()
         {
+
             CreateBoardCommand = ReactiveCommand.Create(SwitchToDiagramPage);
             OpenOptionPane = ReactiveCommand.CreateFromTask(OpenPane);
+
+
+            NextPage = ReactiveCommand.CreateFromTask(ToNextPage);
+            PreviousPage = ReactiveCommand.CreateFromTask(ToPreviousPage);
+            LastPage = ReactiveCommand.CreateFromTask(ToLastPage);
+            FirstPage = ReactiveCommand.CreateFromTask(ToFirstPage);
+
+
+
+
+
+
+            _parsedLogs = new ObservableCollection<ParsedLog>();
+
+            ParsedLog parsedLog = new ParsedLog("wdawd", "wadadwddddd");
+            ParsedLog parsedLog1 = new ParsedLog("wdawd", "wadadwddddd");
+
+            parsedLog.Message = "dddddddddd";
+            parsedLog.GroupedLogs = new List<ParsedLog>();
+            parsedLog1.Message = "aaaaa";
+            parsedLog1.IsHidden = true;
+            parsedLog.GroupedLogs.Add(parsedLog1);
+
+            _parsedLogs.Add(parsedLog);
+
 
             LogsSource = new HierarchicalTreeDataGridSource<ParsedLog>(_parsedLogs)
             {
                 Columns =
                 {
-                    new CheckBoxColumn<ParsedLog>("Скрыть", x=>x.IsHidden),
-                    new HierarchicalExpanderColumn<ParsedLog>(
-                        new TextColumn<ParsedLog, string>("Сообщение", x => x.Message), x=>x.GroupedLogs),
-
+                    new CheckBoxColumn<ParsedLog>("Скрыть", x=>x.IsHidden, (x, value) =>{x.IsHidden = value;  }),
+                    new HierarchicalExpanderColumn<ParsedLog>(new TextColumn<ParsedLog, string>("Время", x => x.TimestampStr), x=>x.GroupedLogs),
+                    new TextColumn<ParsedLog, string>("Сообщение", x => x.Message)
                 },
             };
-                
+
+            
         }
 
-        /*
- <DataGridCheckBoxColumn Header="Скрыть" Binding="{Binding IsHidden}" Width="0.15*"/>
-					<DataGridTextColumn Header="Время" Binding="{Binding Timestamp}" Width="0.4*"/>
-					<DataGridTextColumn Header="Вид лога" Binding="{Binding Level}" Width="0.2*"/>
-					<DataGridTemplateColumn Header="Содержание" Binding="{Binding Message}" Width="0.5*"/>
- */
+
+
+
+
+
+
+
+
+
+        public async Task ToNextPage()
+        {
+            CurrentPage = Math.Clamp(CurrentPage++, 0, MaxPage);
+        }
+        public async Task ToPreviousPage()
+        {
+            CurrentPage = Math.Clamp(CurrentPage--, 0, MaxPage);
+
+        }
+        public async Task ToLastPage()
+        {
+            CurrentPage = Math.Clamp(CurrentPage++, 0, MaxPage);
+
+        }
+        public async Task ToFirstPage()
+        {
+            CurrentPage = Math.Clamp(CurrentPage--, 0, MaxPage);
+        }
+
+
+
+
+
+
 
         private async Task OpenPane()
         {
