c7066225ab2d3997da8214c335fe2ba66dee0cc9
Yupi <youngperchik2404@gmail.com>
2025-10-01 11:14:10 +0500

    started creating colorful logs #14

--- client/client/ViewModels/LogTableViewModel.cs
+++ client/client/ViewModels/LogTableViewModel.cs
@@ -15,18 +15,31 @@ namespace client.ViewModels
     {
         public ReactiveCommand<Unit, Unit> CreateBoardCommand { get; set; }
 
+        private readonly ReactiveList<LogEntry> _logEntries;
 
         public LogTableViewModel()
         {
-            CreateBoardCommand = ReactiveCommand.Create(SwitchToDiagramPage);
+            _logEntries = new ReactiveList<LogEntry>();
+
+            InitializeSampleData();
         }
 
+        public IReactiveList<LogEntry> LogEntries => _logEntries;
 
-        private void SwitchToDiagramPage()
+
+        private void InitializeSampleData()
         {
-            // Blank
-        }
+            var logs = new List<LogEntry>
+        {
+            new() { Time = DateTime.Now, Type = "info", Content = "Application started" },
+            new() { Time = DateTime.Now, Type = "debug", Content = "Debug information" },
+            new() { Time = DateTime.Now, Type = "warn", Content = "Warning message" },
+            new() { Time = DateTime.Now, Type = "error", Content = "Error occurred" },
+            new() { Time = DateTime.Now, Type = "trace", Content = "Trace details" }
+        };
 
+            _logEntries.AddRange(logs);
+        }
 
     }
 
