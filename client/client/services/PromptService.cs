46888edf8c6fad499308654abc7bf9d0cd71389d
Echo_1-1 <fya17102007@gmail.com>
2025-10-02 14:37:33 +0500

    Merge Den

--- client/client/services/PromptService.cs
+++ client/client/services/PromptService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
